b7bb6bddcb5e1e234fe168522c203edc23be40fd
AhmadIkbalDjaya <ikbaldjaya@gmail.com>
2023-10-17 11:45:55 +0800

    04. Bloc Builder

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:bloc/bloc.dart';
-import 'package:flutter/material.dart';
+import "package:belajar_bloc/pages/home.dart";
+import "package:flutter/material.dart";
 
 void main() {
   runApp(const MyApp());
@@ -12,100 +12,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: HomePage(),
-    );
-  }
-}
-
-class CounterCubit extends Cubit<int> {
-  CounterCubit({this.inisialData = 0}) : super(inisialData);
-
-  int inisialData;
-  int? current;
-  int? next;
-
-  void tambahData() {
-    emit(state + 1);
-  }
-
-  void kurangData() {
-    emit(state - 1);
-  }
-
-  // Observer
-  // Fitur Bloc untuk memantau data
-  @override
-  void onChange(Change<int> change) {
-    super.onChange(change);
-    print(change);
-    current = change.currentState;
-    next = change.nextState;
-  }
-
-  @override
-  void onError(Object error, StackTrace stackTrace) {
-    super.onError(error, stackTrace);
-    print(error);
-  }
-}
-
-class HomePage extends StatelessWidget {
-  CounterCubit myCounter = CounterCubit();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("CUBIT APPS"),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          StreamBuilder(
-            initialData: myCounter.inisialData,
-            stream: myCounter.stream,
-            builder: (context, snapshot) {
-              return Center(
-                child: Column(
-                  children: [
-                    Text(
-                      "${snapshot.data}",
-                      style: const TextStyle(fontSize: 50),
-                    ),
-                    const SizedBox(height: 10),
-                    Text(
-                      "Current : ${myCounter.current}",
-                      style: const TextStyle(fontSize: 50),
-                    ),
-                    const SizedBox(height: 10),
-                    Text(
-                      "Next : ${myCounter.next}",
-                      style: const TextStyle(fontSize: 50),
-                    ),
-                  ],
-                ),
-              );
-            },
-          ),
-          const SizedBox(height: 20),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              IconButton(
-                onPressed: () {
-                  myCounter.kurangData();
-                },
-                icon: const Icon(Icons.remove),
-              ),
-              IconButton(
-                onPressed: () {
-                  myCounter.tambahData();
-                },
-                icon: const Icon(Icons.add),
-              ),
-            ],
-          )
-        ],
-      ),
     );
   }
 }
